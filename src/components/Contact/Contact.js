32bccc7bb5d9c06c154834ccc080fbdb6c233d41
Brett Roeder <74941938+mirewolf@users.noreply.github.com>
2021-06-01 16:18:10 -0700

    updated files

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -23,8 +23,8 @@ const Contact = () => ({
         <PortfolioWrapper>
           <PortfolioCard>
             <PortfolioIcon src={Icon1} />
-            <a href="https://sksmejn.github.io/word-force/"">Legend of Worda</a>
-            {/* <PortfolioH2>Pawsperity</PortfolioH2> */}
+            <a href="https://sksmejn.github.io/word-force/">Legend of Worda</a>
+            {/* <PortfolioH2></PortfolioH2> */}
             <PortfolioP>
               The ancient meanings behind magical words.
             </PortfolioP>
@@ -32,7 +32,7 @@ const Contact = () => ({
           <PortfolioCard>
             <PortfolioIcon src={Icon2} />
             <a href="https://boiling-woodland-69446.herokuapp.com/home">Release the Kringle!</a>
-            {/* <PortfolioH2>Busy Parent Scheduler</PortfolioH2> */}
+            {/* <PortfolioH2></PortfolioH2> */}
             <PortfolioP>
               The best way to get a kringle in your stomach.
             </PortfolioP>
